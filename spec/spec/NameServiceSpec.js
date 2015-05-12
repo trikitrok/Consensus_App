2af324d4bc0b4b330550f4558dba394894531fb4
xaviuzz <xaviuzz@gmail.com>
2015-05-12 22:36:21 +0200

    Name Service

---
+++ spec/spec/NameServiceSpec.js
@@ -0,0 +1,62 @@
+describe("NameService", function() {
+    
+    FIRST_NAMES = [
+      "Jon","Alfred", "Mary"
+    ];
+
+    MIDDLE_NAMES = [
+      "Jon","Alfred", "Mary"
+    ];
+
+    LAST_NAMES = [
+      "Jon","Alfred", "Mary"
+    ];
+
+    var aNameService;
+    var baseURL = "http://www.google.com";
+    var SECOND_ARGUMENT = 1
+
+
+    beforeEach(function() {
+        
+        aNameService = new Names();
+        aNameService.setBaseURL(baseURL);
+    });
+
+
+    it("inherits Service", function() {
+        expect(aNameService).toEqual(jasmine.any(CUORE.Service));
+    });
+
+    it('composes a three part randomized name', function() {
+      var generatedName = retrieveGeneratedName();
+      expect(generatedName.split(" ").length).toEqual(3);
+
+    });
+
+    it('gets the name from the apropiate list', function() {
+      var generatedName = retrieveGeneratedName();
+      var name = generatedName.split(" ")[0]
+      expect(FIRST_NAMES).toContain(name);
+    });
+
+    it('gets the middle name from the apropiate list', function() {
+      var generatedName = retrieveGeneratedName();
+      var middle = generatedName.split(" ")[1]
+      expect(MIDDLE_NAMES).toContain(middle);
+    });
+
+    it('gets the last name from the apropiate list', function() {
+      var generatedName = retrieveGeneratedName();
+      var last = generatedName.split(" ")[2]
+      expect(LAST_NAMES).toContain(last);
+    });
+
+    var retrieveGeneratedName = function() {
+      spyOn(CUORE.Bus, "emit");
+      aNameService.execute("generate",null);
+      
+      var generatedName = CUORE.Bus.emit.calls.mostRecent().args[SECOND_ARGUMENT];
+      return generatedName;
+    };
+});
